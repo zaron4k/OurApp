a71b6b1eea871fc8e24429d69e1d52398fd7ef31
zaron4k <76218026+zaron4k@users.noreply.github.com>
2026-09-25 16:27:01 +0300

    изменение

--- OurApp/Form1.cs
+++ OurApp/Form1.cs
@@ -19,7 +19,7 @@ namespace OurApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Добро пожаловать!");
+            MessageBox.Show("Доброе утро!");
         }
     }
 }
